d6ce49843283e52c12ae8214aa1987c6f5ade21e
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-04-24 15:19:17 -0700

    add useState to control text on page, move style for a to jsx global

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,13 @@
 import Head from 'next/head'
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Hi, it's Darren!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="container">
-        <h1 className="title">HELLO</h1>
-
-        <div className="circle">
-          <img className="computer" src="/images/computer.png" alt="An open laptop"/>
-          <img className="phone" src="/images/phone.png" alt="An old brick cell phone from the 80's"/>
-
-        </div>
-        <p className="info-text">
+  const [text, updateText] = useState({
+    title: ["HELLO", "ABOUT"],
+    infoText: [
+      {
+        __html: `
           My name is Darren and I’m a web developer.
           <br />
           <br />
@@ -26,13 +17,51 @@ export default function Home() {
           MongoDB, and other fun things.
           <br />
           <br />
-          Send me an email if you’d like to work together.
+          <a href="mailto:darren.bridenbeck@gmail.com">Send me an email</a> if
+          you’d like to work together.
           <br />
           <br />
-          Check out my latest project for Whidbey Herbal.
-        </p>
-        <span className="aboutLink">
-          about
+          Check out my latest project for
+          <a href="http://whidbeyherbal.com">Whidbey Herbal</a>.
+        `,
+      },
+      {
+        __html: `
+          Although I have a background in some business-bro stuff (sales dev, solutions engineer, account & vendor management, creating documentation), I used to be a life coach which completely blew my mind. I learned how to hone my listening skills and ask good questions to help people figure out their own solutions. I bring all of this to my work as web developer.
+          <br />
+          <br />
+          When I’m not working, I am hanging out with my wife and 18 month old daughter, playing pinball, or rollerskating, 
+        `,
+      },
+    ],
+    linkText: ["about", "hello"],
+  });
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Hi, it's Darren!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container">
+        <h1 className="title">{text.title[0]}</h1>
+
+        <div className="circle">
+          <img
+            className="computer"
+            src="/images/computer.png"
+            alt="An open laptop"
+          />
+          <img
+            className="phone"
+            src="/images/phone.png"
+            alt="An old brick cell phone from the 80's"
+          />
+        </div>
+        <p className="info-text" dangerouslySetInnerHTML={text.infoText[0]}></p>
+        <span className="pageLink">
+          <span className="linkText">{text.linkText[0]}</span>
           <span className="hyphen1"> -</span>
           <span className="hyphen2">-</span>
           <span className="greaterSign">></span>
@@ -52,7 +81,7 @@ export default function Home() {
         .title {
           font-size: 1em;
           font-weight: 300;
-          margin: 10% 0 0 10%;
+          margin: 5% 0 0 10%;
         }
 
         .circle {
@@ -60,7 +89,7 @@ export default function Home() {
           display: block;
           height: 360px;
           width: 360px;
-          margin: 10% auto 0 auto;
+          margin: 5% auto 0 auto;
           border-radius: 50%;
           background-color: #c5ff8a;
         }
@@ -71,27 +100,27 @@ export default function Home() {
           left: -10%;
           top: 20%;
           width: 65%;
-          animation: rotateCW 100s infinite
+          animation: rotateCW 100s infinite;
         }
-        
+
         .phone {
           display: block;
           position: absolute;
           right: -25%;
           top: 12%;
           width: 80%;
-          animation: rotateCCW 120s infinite
+          animation: rotateCCW 120s infinite;
         }
 
         .info-text {
-          font-size: 1em;
-          line-height: 1.125em;
+          font-size: 0.9em;
+          line-height: 1.325em;
           font-weight: 300;
-          margin: 30px auto 0 auto;
+          margin: 20px auto 0 auto;
           width: 85%;
         }
 
-        .aboutLink {
+        .pageLink {
           position: absolute;
           display: inline-block;
           right: 30px;
@@ -101,18 +130,44 @@ export default function Home() {
           color: #8affff;
         }
 
-        @keyframes rotateCW {
-          0% {transform: translateY(0px) rotate(0deg)}
-          15% {transform: rotate(-5deg)}
-          50% {transform: translateY(60px) rotate(45deg)}
-          100% {transform: translateY(0px) rotate(0deg)}
+        @media screen and (min-width: 1270px) {
+          .container {
+            max-width: 650px;
+          }
+
+          .title {
+            margin: 5%;
+          }
         }
-        
+
+        @keyframes rotateCW {
+          0% {
+            transform: translateY(0px) rotate(0deg);
+          }
+          15% {
+            transform: rotate(-5deg);
+          }
+          50% {
+            transform: translateY(60px) rotate(45deg);
+          }
+          100% {
+            transform: translateY(0px) rotate(0deg);
+          }
+        }
+
         @keyframes rotateCCW {
-          0% {transform: translateY(0px) rotate(0deg)}
-          50% {transform: translateY(-120px) rotate(-45deg)}
-          80% {transform: translateY(60px) rotate(5deg)}
-          100% {transform: translateY(0px) rotate(0deg)}
+          0% {
+            transform: translateY(0px) rotate(0deg);
+          }
+          50% {
+            transform: translateY(-120px) rotate(-45deg);
+          }
+          80% {
+            transform: translateY(60px) rotate(5deg);
+          }
+          100% {
+            transform: translateY(0px) rotate(0deg);
+          }
         }
       `}</style>
 
@@ -125,6 +180,10 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+        }
+        a {
+          color: #8affff;
+          text-decoration: none;
         }
 
         * {
