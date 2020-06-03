a6d3680744519afd9bee47d9797fed2243e94705
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-03 07:34:11 -0700

    convert to SC

--- components/CircleAndImages/PinballImage.tsx
+++ components/CircleAndImages/PinballImage.tsx
@@ -1,47 +1,43 @@
+import styled, { keyframes } from 'styled-components';
 import ImageModel from "../../models/images";
 
-const PinballImage: React.FC<ImageModel> = ({ onHelloPage }) => (
-  <>
-    <picture>
-      <source
-        srcSet={require("../../public/images/pinball.png?webp")}
-        type="image/webp"
-      />
-      <source
-        srcSet={require("../../public/images/pinball.png")}
-        type="image/png"
-      />
-      <img
-        className={`pinball ${onHelloPage ? "blueFilter" : "redFilter"}`}
-        srcSet={require("../../public/images/pinball.png")}
-        alt="A pinball table"
-      />
-    </picture>
-    <style jsx>{`
-      .pinball {
-        display: block;
-        position: absolute;
-        margin: 25% 0 0 5%;
-        width: 40%;
-        animation: rotatePinball 80s infinite linear;
-        animation-delay: 100ms;
-        transition: filter 0.5s;
-        -webkit-transform: rotateZ(360deg);
-      }
+const rotate = keyframes`
+  0% {
+    transform: translateY(0px) rotate(0deg);
+  }
+  35% {
+    transform: translateY(-90px) rotate(40deg);
+  }
+  100% {
+    transform: translateY(0px) rotate(0deg);
+  }
+`;
 
-      @keyframes rotatePinball {
-        0% {
-          transform: translateY(0px) rotate(0deg);
-        }
-        35% {
-          transform: translateY(-90px) rotate(40deg);
-        }
-        100% {
-          transform: translateY(0px) rotate(0deg);
-        }
-      }
-    `}</style>
-  </>
-);
+const StyledPinballImage = styled.img`
+  display: block;
+  position: absolute;
+  margin: 25% 0 0 5%;
+  width: 40%;
+  animation: ${rotate} 80s infinite linear;
+  animation-delay: 100ms;
+  transition: filter 0.5s;
+`;
+
+const PinballImage: React.FC<ImageModel> = ({ onHelloPage }) => 
+  <picture>
+    <source
+      srcSet={require("../../public/images/pinball.png?webp")}
+      type="image/webp"
+    />
+    <source
+      srcSet={require("../../public/images/pinball.png")}
+      type="image/png"
+    />
+    <StyledPinballImage
+      className={`pinball ${onHelloPage ? "blueFilter" : "redFilter"}`}
+      srcSet={require("../../public/images/pinball.png")}
+      alt="A pinball table"
+    />
+  </picture>
 
 export default PinballImage;
