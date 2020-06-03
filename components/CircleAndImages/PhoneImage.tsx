9c464b1686e3ff395b4aa6fdb6b9816c106b627d
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-03 07:32:01 -0700

    convert to SC

--- components/CircleAndImages/PhoneImage.tsx
+++ components/CircleAndImages/PhoneImage.tsx
@@ -1,48 +1,44 @@
+import styled, { keyframes } from 'styled-components';
 import ImageModel from "../../models/images";
 
-const PhoneImage: React.FC<ImageModel> = ({ onHelloPage }) => (
-  <>
-    <picture>
-      <source
-        srcSet={require("../../public/images/phone.png?webp")}
-        type="image/webp"
-      />
-      <source
-        srcSet={require("../../public/images/phone.png")}
-        type="image/png"
-      />
-      <img
-        className={`phone ${onHelloPage ? "blueFilter" : "redFilter"}`}
-        srcSet={require("../../public/images/phone.png")}
-        alt="An 80's style mobile phone"
-      />
-    </picture>
-    <style jsx>{`
-      .phone {
-        display: block;
-        position: absolute;
-        margin: 25% 0px 0px 60%;
-        width: 35%;
-        height: auto;
-        animation: rotatePhone 90s infinite linear;
-        animation-delay: 300ms;
-        transition: filter 0.5s;
-        -webkit-transform: rotateZ(360deg);
-      }
+const rotate = keyframes`
+  0% {
+    transform: translateY(0px) rotate(0deg);
+  }
+  80% {
+    transform: translateY(60px) rotate(45deg);
+  }
+  100% {
+    transform: translateY(0px) rotate(0deg);
+  }
+`;
 
-      @keyframes rotatePhone {
-        0% {
-          transform: translateY(0px) rotate(0deg);
-        }
-        80% {
-          transform: translateY(60px) rotate(45deg);
-        }
-        100% {
-          transform: translateY(0px) rotate(0deg);
-        }
-      }
-    `}</style>
-  </>
-);
+const StyledPhoneImage = styled.img`
+    display: block;
+    position: absolute;
+    margin: 25% 0px 0px 60%;
+    width: 35%;
+    height: auto;
+    animation: ${rotate} 90s infinite linear;
+    animation-delay: 300ms;
+    transition: filter 0.5s;
+`;
+
+const PhoneImage: React.FC<ImageModel> = ({ onHelloPage }) =>
+  <picture>
+    <source
+      srcSet={require("../../public/images/phone.png?webp")}
+      type="image/webp"
+    />
+    <source
+      srcSet={require("../../public/images/phone.png")}
+      type="image/png"
+    />
+    <StyledPhoneImage
+      className={`phone ${onHelloPage ? "blueFilter" : "redFilter"}`}
+      srcSet={require("../../public/images/phone.png")}
+      alt="An 80's style mobile phone"
+    />
+  </picture>
 
 export default PhoneImage;
