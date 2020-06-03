e04703b1f3744ae0ed12d798205cd42a7d4b259e
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-03 07:40:34 -0700

    convert to SC

--- components/CircleAndImages/Circle.tsx
+++ components/CircleAndImages/Circle.tsx
@@ -1,40 +1,28 @@
+import styled from 'styled-components';
 import CircleAnimation from "../../animations/CircleAnimation";
+
+const StyledCircle = styled.div`
+  position: relative;
+  display: block;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  border-radius: 50%;
+  transition: all 0.3s;
+  background-color: ${({ color }) => (color === "red" ? "#d13b40" : "#3bc9d1")};
+`;
 
 interface CircleProps {
   onHelloPage: boolean;
   pageClickedOnce: boolean;
 }
 
-const Circle: React.FC<CircleProps> = ({ onHelloPage, pageClickedOnce }) => {
-  return (
-    <>
-      <CircleAnimation
-        onHelloPage={onHelloPage}
-        pageClickedOnce={pageClickedOnce}
-      >
-        <div className={`circle ${onHelloPage ? "red" : "blue"}`}></div>
-      </CircleAnimation>
-      <style jsx>{`
-        .circle {
-          position: relative;
-          display: block;
-          height: 100%;
-          width: 100%;
-          margin: 0 auto;
-          border-radius: 50%;
-          transition: all 0.3s;
-        }
-
-        .blue {
-          background-color: #3bc9d1;
-        }
-
-        .red {
-          background-color: #d13b40;
-        }
-      `}</style>
-    </>
-  );
-};
+const Circle: React.FC<CircleProps> = ({ onHelloPage, pageClickedOnce }) => 
+  <CircleAnimation
+    onHelloPage={onHelloPage}
+    pageClickedOnce={pageClickedOnce}
+  >
+    <StyledCircle color={onHelloPage ? "red" : "blue"}></StyledCircle>
+  </CircleAnimation>
 
 export default Circle;
