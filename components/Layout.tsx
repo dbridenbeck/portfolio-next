1aefd6105913cc119d64674f4642bf559def3541
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-10 06:15:02 -0700

    add padding to Layout, remove flexbox from MainContent

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 import { ReactElement } from 'react';
 
 const StyledLayout = styled.div`
-  display: flex;
-  flex-direction: column;
   position: relative;
   margin: 0 auto;
+  padding: 2.667%;
   height: 100%;
   max-width: 650px;
-  color: white;
 `;
 
 interface LayoutProps {
