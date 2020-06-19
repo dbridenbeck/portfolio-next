eebf64129aded53aa671bc7a0b1a6e2a1863792a
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-18 17:31:21 -0700

    update copy

--- state/initState.tsx
+++ state/initState.tsx
@@ -32,7 +32,7 @@ const initState: AppStateModel = {
       type: "eCommerce Website",
       tech: ["React", "Redux", "Styled-Components", "Shopify API"],
       projectDetail:
-        "The client's goal was to limit the their reliance on Shopify by having a custom site and use images to better tell their story. I'm most proud of how I implemented a design file into a pixel-perfect responsive site and managed state for checkout. Future Roadmap is to migrate to Nextjs and add route animations.",
+        "The client's goal was to limit their reliance on Shopify by having a custom site and use images to tell their story. It was fun implementing a design file into a pixel-perfect responsive site and managing state for checkout. Roadmap is to migrate to Nextjs and add route animations.",
       url: "http://whidbeyherbal.com",
       color: "#9686ef",
       webm: `${whidbeyHerbalWebm}`,
